4822aabe43ffc62c84f4f84aae5be4179ae3c3fd
cnouguier <ch.nouguier@gmail.com>
2026-04-21 12:22:17 +0200

    ci: added scripts

---
+++ scripts/init_runner.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+JOB_ID=$1
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+
+. "$THIS_DIR/kash/kash.sh"
+
+### Github Actions
+
+init_github_run_tests() {
+    install_reqs age sops nvm node20 mongo7 sonar_scanner_cli
+    sudo apt-get install gdal-bin
+}
+
+init_github_additional_tests() {
+    install_reqs age sops nvm node24 mongo7 mongo8
+    sudo apt-get install gdal-bin
+}
+
+init_github_build_docs() {
+    install_reqs age sops nvm node20
+}
+
+begin_group "Init $CI_ID for $JOB_ID"
+
+init_"${CI_ID}_${JOB_ID}"
+
+end_group "Init $CI_ID for $JOB_ID"
